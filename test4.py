01d163b88bfcd933a27c8d6dcc0b1ffd5fccef15
atila-b <atilabernardo1@academico.ufs.br>
2023-09-22 18:09:07 +0000

    Add função para inserir codigo de maquina na section .text

--- test4.py
+++ test4.py
@@ -3,26 +3,6 @@ from elftools.elf.elffile import ELFFile
 from keystone import *
 from iced_x86 import *
 
-# Inicialize o Keystone com a arquitetura x86_64
-ks = Ks(KS_ARCH_X86, KS_MODE_64)
-
-def disassemble_binary(binary_data):
-
-    decoder = Decoder(64, binary_data, 0)
-    formatter = FastFormatter()
-    code = bytearray()
-
-    # Monta o código de máquina a partir do arquivo binário
-    for instr in decoder:
-        disassembly = formatter.format(instr)
-        print(disassembly)
-        if disassembly != "(bad)":
-            try:
-                bytecode, _ = ks.asm(disassembly)
-                code.extend(bytes(bytecode))
-            except Exception as e:
-                print(e)
-
 def extract_edit_save_text_section(input_file_path, output_file_path):
     # Abra o arquivo binário em modo de leitura.
     with open(input_file_path, 'rb') as file:
@@ -43,22 +23,30 @@ def extract_edit_save_text_section(input_file_path, output_file_path):
     section_end   = section_start + elf_section['sh_size']
     
     # Insira instrução na seção .text
-    text_data = insert_instruction_in_position(text_data, b'\x90', 11)
-    #disassemble_binary(text_data)
-    print(text_data)
+    for i in range(0, len(text_data)):
+        new_text_data = insert_instruction_in_position(text_data, b'\x90\x90\x90', i)
+        
+        # Modificação dos dados do arquivo original
+        new_data = data[:section_start] + new_text_data + data[section_end:]
     
-    # Modificação dos dados do arquivo original
-    data = data[:section_start] + text_data + data[section_end:]
-
-    # Salve as edições de volta no arquivo binário.
-    with open(output_file_path, 'wb') as file:
-        file.write(data)
+        # Salve as edições de volta no arquivo binário.
+        with open(output_file_path, 'wb') as file:
+            file.write(new_data)
+        
+        # Defina as permissões de execução no arquivo editado.
+        os.chmod(output_file_path, 0o777)  
+        
+        # Executa o arquivo e verifica se o retorno está correto.
+        if(exec_bin() == 0):
+            print(new_text_data)
+            break
     
-    # Defina as permissões de execução no arquivo editado.
-    os.chmod(output_file_path, 0o777)  # 0o777 representa permissões rwxr-xr-x (r = read, w = write, x = execute)
-    
+# Insere código x86_64 em uma posição específica do bytearray
 def insert_instruction_in_position(code, inst, position):
-    return code[:position] + inst + code[position:]
+    code = bytearray(code)
+    code[position:position+len(inst)] = inst
+    return bytes(code)
+
     
 # Executa arquivo binário
 import subprocess
@@ -87,4 +75,3 @@ def exec_bin():
 input_file_path = 'hello.bin'
 output_file_path = 'helloM.bin'
 extract_edit_save_text_section(input_file_path, output_file_path)
-exec_bin()
