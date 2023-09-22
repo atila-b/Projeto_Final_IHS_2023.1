cb5346293db7e45e1c2bb839a2be054def0da4c0
atila-b <atilabernardo1@academico.ufs.br>
2023-09-22 18:50:19 +0000

    Insire instruções em posições aleatórias do arquivo binário até a execução ser bem sucedida

--- test.py
+++ test.py
@@ -1,45 +1,80 @@
-import struct
+import os
+import random
 from elftools.elf.elffile import ELFFile
-from elftools.elf.sections import Section
+from keystone import *
+from iced_x86 import *
 
-def extract_text_section(elf_filename, output_filename):
-    with open(elf_filename, 'rb') as elf_file:
-        elf = ELFFile(elf_file)
-        text_section = None
-
-        # Encontre a seção .text
-        for section in elf.iter_sections():
-            if section.name == '.text':
-                text_section = section
-                break
-
-        if text_section:
-            # Extraia os dados da seção .text
-            text_data = text_section.data()
-            
-            # Escreva os dados modificados no arquivo ELF original
-            elf_file.seek(text_section['sh_offset'])
-            
-            # Insira instrução na seção .text
-            text_data = insert_instruction_in_position(text_data, b'\x90\x90\x90\x90', 0)
-            
-            print(text_data)
-            #elf_file.write(text_data)
-            #print(f'Instrução inserida na posição {hex(address)} na seção .text e salva no arquivo ELF.')
-            
-            # Escreva os dados em um arquivo de saída
-            with open(output_filename, 'wb') as output_file:
-                output_file.write(text_data)
-            print(f'Seção .text extraída e salva em {output_filename}')
-        else:
-            print('Seção .text não encontrada no arquivo ELF.')
-            
-def insert_instruction_in_position(code, inst, position):
-    return code[:position] + inst + code[position:]
+# Extrai a section .text do arquivo e insere instruções nela até que a execução funcione
+def extract_edit_save_text_section(input_file_path, output_file_path):
+    # Abra o arquivo binário em modo de leitura.
+    with open(input_file_path, 'rb') as file:
+        # Leitura do arquivo binário
+        data = file.read()
+        
+        # Get objeto ELF do arquivo binário
+        elf = ELFFile(file)
     
+        # Get seção .text do ELF
+        elf_section = elf.get_section_by_name(".text") 
+        
+        # Get binário da seção .text
+        text_data = elf_section.data()
+                                           
+    # Encontre a posição inicial e final da seção .text
+    section_start = elf_section['sh_offset'] 
+    section_end   = section_start + elf_section['sh_size']
+    
+    # Insira instruções na seção .text até a execução ser bem sucedida
+    while 1:
+        # Insira instrução em posição aleatória da section .text
+        new_text_data = insert_instruction_in_position(text_data, b'\x90\x90\x90', random.randint(0, len(text_data)))
+        
+        # Modificação dos dados do arquivo original
+        new_data = data[:section_start] + new_text_data + data[section_end:]
+    
+        # Salve as edições de volta no arquivo binário.
+        with open(output_file_path, 'wb') as file:
+            file.write(new_data)
+        
+        # Defina as permissões de execução no arquivo editado.
+        os.chmod(output_file_path, 0o777)  
+        
+        # Execute o arquivo e verifica se o retorno está correto.
+        if(exec_bin() == 0):
+            print(new_text_data)
+            break
+    
+# Insere código x86_64 em uma posição específica do bytearray
+def insert_instruction_in_position(code, inst, position):
+    code = bytearray(code)
+    code[position:position+len(inst)] = inst
+    return bytes(code)
 
-if __name__ == '__main__':
-    elf_filename = 'helloM.bin'  # Substitua pelo nome do seu arquivo ELF
-    output_filename = 'text_section.bin'  # Nome do arquivo de saída
+    
+# Executa arquivo binário
+import subprocess
 
-    extract_text_section(elf_filename, output_filename)
+def exec_bin():
+    arquivo_binario = 'helloM.bin'
+    
+    # Comando para executar o arquivo binário.
+    comando = f'./{arquivo_binario}' 
+    
+    # Tente executar o arquivo binário e capturar a saída.
+    try:
+        resultado = subprocess.run(comando, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+        saida = resultado.stdout
+        print(f"Saída do arquivo binário:\n{saida}")
+        return 0
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o arquivo binário: {e}")
+        return -1
+    except FileNotFoundError:
+        print(f"O arquivo binário '{arquivo_binario}' não foi encontrado.")
+        return -1
+
+
+# Main
+input_file_path = 'hello.bin'
+output_file_path = 'helloM.bin'
+extract_edit_save_text_section(input_file_path, output_file_path)
