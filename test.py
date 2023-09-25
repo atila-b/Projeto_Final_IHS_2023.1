9e3d16b6f996ef9371b72fa97767baa5dbeaf8ea
atila-b <atilabernardo1@academico.ufs.br>
2023-09-25 17:21:12 +0000

    Inicialização da população de indivíduos do algoritmo genético

--- test.py
+++ test.py
@@ -4,6 +4,29 @@ from elftools.elf.elffile import ELFFile
 from keystone import *
 from iced_x86 import *
 
+class Individual:
+    def __init__(self, text_section, init_inst):
+        self.text_section = text_section
+        self.obfuscation_insts = [init_inst]
+        self.fit_value = 0
+
+class Model:
+    def __init__(self, population_size, generations):
+        self.population_size = population_size
+        self.generations = generations
+        self.population = []
+        self.top_individual = None
+        
+    def init_population(self):
+        for i in range(self.population_size):
+          # Gere um indivíduo
+          text_section, init_inst = extract_edit_save_text_section(input_file_path, output_file_path)
+          individual = Individual(text_section=text_section, init_inst=init_inst)
+          individual.fit_value += 1
+    
+          # Insira o indivíduo na população
+          self.population.append(individual)
+
 # Extrai a section .text do arquivo e insere instruções nela até que a execução funcione
 def extract_edit_save_text_section(input_file_path, output_file_path):
     # Abra o arquivo binário em modo de leitura.
@@ -26,8 +49,12 @@ def extract_edit_save_text_section(input_file_path, output_file_path):
     
     # Insira instruções na seção .text até a execução ser bem sucedida
     while 1:
+        # Instrução a ser inserida na section .text
+        inst = b'\x90\x90\x90'
+        position = random.randint(0, len(text_data))
+        
         # Insira instrução em posição aleatória da section .text
-        new_text_data = insert_instruction_in_position(text_data, b'\x90\x90\x90', random.randint(0, len(text_data)))
+        new_text_data = insert_instruction_in_position(text_data, inst, position)
         
         # Modificação dos dados do arquivo original
         new_data = data[:section_start] + new_text_data + data[section_end:]
@@ -41,7 +68,8 @@ def extract_edit_save_text_section(input_file_path, output_file_path):
         
         # Execute o arquivo e verifica se o retorno está correto.
         if(exec_bin() == 0):
-            print(new_text_data)
+            #print(new_text_data)
+            return new_text_data, (inst, position)
             break
     
 # Insere código x86_64 em uma posição específica do bytearray
@@ -67,10 +95,10 @@ def exec_bin():
         print(f"Saída do arquivo binário:\n{saida}")
         return 0
     except subprocess.CalledProcessError as e:
-        print(f"Erro ao executar o arquivo binário: {e}")
+        #print(f"Erro ao executar o arquivo binário: {e}")
         return -1
     except FileNotFoundError:
-        print(f"O arquivo binário '{arquivo_binario}' não foi encontrado.")
+        #print(f"O arquivo binário '{arquivo_binario}' não foi encontrado.")
         return -1
 
 
@@ -78,3 +106,7 @@ def exec_bin():
 input_file_path = 'hello.bin'
 output_file_path = 'helloM.bin'
 extract_edit_save_text_section(input_file_path, output_file_path)
+
+# Algoritmo genético
+model = Model(population_size=100, generations=1000)
+model.init_population()
